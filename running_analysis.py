20e218b8eeeef4e13e3c28d5227529dd9549da7c
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-07 11:11:24 +0200

    asd

--- running_analysis.py
+++ running_analysis.py
@@ -1,12 +1,11 @@
 import cv2
 from ultralytics import YOLO
 
-
 model = YOLO("yolov8m-pose.pt")
-
 
 video_source = "assets/running.mp4"
 cap = cv2.VideoCapture(video_source)
+
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -14,11 +13,13 @@ while cap.isOpened():
     if success:
         results = model(
             source=frame,
-            conf = 0.25
+            conf=0.5
         )
 
-        annotated_frame = results[0].plot()
+        annotated_frame = results[0].plot(boxes=False)
 
+
+        cv2.imshow("Sports analysis", annotated_frame)
         key = cv2.waitKey(1)
 
         if key == 113:
