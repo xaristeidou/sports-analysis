c4656c1b7361d6470946585a71aaa43c0ad41ba0
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-07 10:36:38 +0200

    added stream-loop and prediction of model

--- running_analysis.py
+++ running_analysis.py
@@ -4,3 +4,18 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8m-pose.pt")
 
+
+video_source = "assets/video_path.mp4"
+cap = cv2.VideoCapture(video_source)
+
+while cap.isOpened():
+    success, frame = cap.read()
+
+    if success:
+        results = model(
+            source=frame,
+            conf = 0.25
+        )
+
+        annotated_frame = results[0].plot()
+    
